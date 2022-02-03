2fec036c78c676d9859c8005017d647be28babab
getBoolean <danielwnewville@gmail.com>
2022-02-03 17:56:02 -0600

    Create constants for properties

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+class EpubMasterConstants {
+  static const kAccessModeProperty = 'accessMode';
+  static const kAccessibilityFeatureProperty = 'accessibilityFeature';
+  static const kAccessibilitySummaryProperty = 'accessibilitySummary';
+  static const kAccessibilityHazardProperty = 'accessibilityHazard';
+  static const kAccessModeSufficientProperty = 'accessModeSufficient';
+  static const kAccessibilityAPIProperty = 'accessibilityAPI';
+  static const kAccessibilityControlProperty = 'accessibilityControl';
+}
--- lib/epub_master.dart
+++ lib/epub_master.dart
@@ -1,4 +1,5 @@
 library epub_master;
 
+export 'constants.dart';
 export 'enums/enums.dart';
 export 'models/models.dart';
