792f921454ed2860b9a83e69393e00d34672b086
getBoolean <danielwnewville@gmail.com>
2022-02-04 16:03:23 -0600

    Create Epub

---
+++ lib/models/epub.dart
@@ -0,0 +1,9 @@
+import '../epub_master.dart';
+
+class Epub {
+  Epub({
+    required this.packageMetadata,
+  });
+
+  final PackageMetadata packageMetadata;
+}
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,4 @@
+export 'epub.dart';
 export 'media_types.dart';
 export 'opf/epub_package.dart';
 export 'opf/epub_package_identity.dart';
