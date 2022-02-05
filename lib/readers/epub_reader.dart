43afc64fd15ea8f758b6484b4e4150f60fda3ed5
getBoolean <danielwnewville@gmail.com>
2022-02-04 19:13:01 -0600

    Check if archive is an epub

--- lib/readers/epub_reader.dart
+++ lib/readers/epub_reader.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 import 'package:archive/archive.dart';
 
 import '../epub_master.dart';
+import '../utils/file_utils.dart';
 
 class EpubReader {
   final Archive archive;
@@ -10,6 +11,11 @@ class EpubReader {
       : archive = ArchiveService.instance.decodeZip(bytes);
 
   Epub readBook() {
+    final isEpub = FileUtils.isEpubFile(archive);
+    if (!isEpub) {
+      throw EpubException('File was not an EPUB');
+    }
+
     final packageMetadata = PackageReader().readMetadata(archive);
     return Epub(packageMetadata: packageMetadata);
   }
