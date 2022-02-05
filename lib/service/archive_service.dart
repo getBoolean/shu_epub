680cfcd0d8cd06c92ce75ba1ac6378a8befc56c5
getBoolean <danielwnewville@gmail.com>
2022-02-04 21:36:21 -0600

    ArchiveService uses static methods

--- lib/service/archive_service.dart
+++ lib/service/archive_service.dart
@@ -2,17 +2,15 @@ import 'dart:typed_data';
 import 'package:archive/archive.dart';
 
 class ArchiveService {
-  static final ArchiveService instance = ArchiveService();
-
   /// Decode epub data
   ///
   /// TODO: Implement using Isolate (or Worker on web)
-  Archive decodeZip(Uint8List bytes) {
+  static Archive decodeZip(Uint8List bytes) {
     return ZipDecoder().decodeBytes(bytes);
   }
 
   /// Get OPF file from Archive
-  ArchiveFile? getOpfPackageDocument(Archive archive) {
+  static ArchiveFile? getOpfPackageDocument(Archive archive) {
     // According to the epub specs, the Opf file SHOULD end in `.opf` but it is not required.
     // TODO: If a `.opf` file is not found, instead search for a file with mime type `application/oebps-package+xml`
     for (final ArchiveFile file in archive.files) {
