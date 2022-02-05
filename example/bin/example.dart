13a8ecf1307673128013f7b29e6dfdfd12695af0
getBoolean <danielwnewville@gmail.com>
2022-02-04 18:13:57 -0600

    Fix example

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,7 +1,16 @@
-import 'package:example/parse_epub.dart';
+import 'dart:io';
+import 'package:epub_master/epub_master.dart';
+import 'package:path/path.dart' as p;
 
 Future<void> main(List<String> arguments) async {
   print('Hello world!');
 
-  await parseEpub();
+  
+  final filePath = p.join(Directory.current.path, 'assets', 'Guardians.epub');
+
+  final File file = File(filePath);
+
+  final reader = EpubReader(file.readAsBytesSync());
+  final book = reader.readBook();
+  print(book.packageMetadata.toString());
 }
