ea374bb0172064991e2c332f1518ccf75889a7ac
getBoolean <danielwnewville@gmail.com>
2022-02-04 23:39:57 -0600

    Add EpubContainerFile containerFile to Epub and create data class methods

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -5,12 +5,11 @@ import 'package:path/path.dart' as p;
 Future<void> main(List<String> arguments) async {
   print('Hello world!');
 
-  
   final filePath = p.join(Directory.current.path, 'assets', 'Guardians.epub');
 
   final File file = File(filePath);
 
   final reader = EpubReader(file.readAsBytesSync());
   final book = reader.readBook();
-  print(book.packageMetadata.toString());
+  print(book);
 }
--- lib/models/epub.dart
+++ lib/models/epub.dart
@@ -1,9 +1,49 @@
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
 import '../epub_master.dart';
 
-class Epub {
+class Epub extends Equatable {
   Epub({
     required this.packageMetadata,
+    required this.containerFile,
   });
 
   final PackageMetadata packageMetadata;
+  final EpubContainerFile containerFile;
+
+  Epub copyWith({
+    PackageMetadata? packageMetadata,
+    EpubContainerFile? containerFile,
+  }) {
+    return Epub(
+      packageMetadata: packageMetadata ?? this.packageMetadata,
+      containerFile: containerFile ?? this.containerFile,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'packageMetadata': packageMetadata.toMap(),
+      'containerFile': containerFile.toMap(),
+    };
+  }
+
+  factory Epub.fromMap(Map<String, dynamic> map) {
+    return Epub(
+      packageMetadata: PackageMetadata.fromMap(map['packageMetadata']),
+      containerFile: EpubContainerFile.fromMap(map['containerFile']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Epub.fromJson(String source) => Epub.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Epub(packageMetadata: $packageMetadata, containerFile: $containerFile)';
+
+  @override
+  List<Object> get props => [packageMetadata, containerFile];
 }
