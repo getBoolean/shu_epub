e44dc41fb99c84046d30aa9d05faa211cb014984
getBoolean <danielwnewville@gmail.com>
2022-02-06 02:13:32 -0600

    use temp empty constructor

--- lib/readers/package_reader.dart
+++ lib/readers/package_reader.dart
@@ -5,7 +5,7 @@ import '../epub_master.dart';
 class PackageReader {
   static PackageFile parse(Archive archive, ContainerFile containerFile) {
     return PackageFile(
-      packageIdentity: null,
+      packageIdentity: PackageIdentity.zero(),
     );
   }
 }
