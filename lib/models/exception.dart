e0e4683782bc05eebcb78bea55d12920c90d8f55
getBoolean <danielwnewville@gmail.com>
2022-02-06 18:07:06 -0600

    Add copyWith, toString and props to EpubException

--- lib/models/exception.dart
+++ lib/models/exception.dart
@@ -1,7 +1,29 @@
-class EpubException implements Exception {
-  EpubException(this.message, [this.error, this.stackTrace]);
+import 'package:equatable/equatable.dart';
 
+class EpubException extends Equatable implements Exception {
   final String message;
   final Exception? error;
   final StackTrace? stackTrace;
+
+  // GENERATED DO NOT MODOFY
+
+  EpubException(this.message, [this.error, this.stackTrace]);
+
+  EpubException copyWith({
+    String? message,
+    Exception? error,
+    StackTrace? stackTrace,
+  }) {
+    return EpubException(
+      message ?? this.message,
+      error ?? this.error,
+      stackTrace ?? this.stackTrace,
+    );
+  }
+
+  @override
+  String toString() => 'EpubException(message: $message, error: $error, stackTrace: $stackTrace)';
+
+  @override
+  List<Object> get props => [message, error ?? '', stackTrace ?? ''];
 }
