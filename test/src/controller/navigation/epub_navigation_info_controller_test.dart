a6526e7ccdf3cd4828356ad49702015502d3be6e
getBoolean <danielwnewville@gmail.com>
2022-03-22 23:34:33 -0500

    add tests

--- test/src/controller/navigation/epub_navigation_info_controller_test.dart
+++ test/src/controller/navigation/epub_navigation_info_controller_test.dart
@@ -44,4 +44,84 @@ void main() {
       },
     );
   });
+
+  group('getImage', () {
+    test(
+      'on input without a img element, expect a null value',
+      () async {
+        final input = '''
+  <navLabel>
+  </navLabel>
+  ''';
+        final controller = EpubNavigationInfoController.fromString(input);
+        final actualValue = controller.getImage();
+
+        expect(actualValue, isNull);
+      },
+    );
+
+    test(
+      'on input with a img element, expect a img object',
+      () async {
+        final input = '''
+  <navLabel>
+      <img>
+      </img>
+  </navLabel>
+  ''';
+        final controller = EpubNavigationInfoController.fromString(input);
+        final expectedValue = EpubNavigationImage();
+        final actualValue = controller.getImage();
+
+        expect(actualValue, expectedValue);
+      },
+    );
+  });
+
+  group('getText', () {
+    test(
+      'on input without a text element, expect a null value',
+      () async {
+        final input = '''
+  <navLabel>
+  </navLabel>
+  ''';
+        final controller = EpubNavigationInfoController.fromString(input);
+        final actualValue = controller.getText();
+
+        expect(actualValue, isNull);
+      },
+    );
+
+    test(
+      'on input with a text element with no text, expect an empty string',
+      () async {
+        final input = '''
+  <navLabel>
+      <text></text>
+  </navLabel>
+  ''';
+        final controller = EpubNavigationInfoController.fromString(input);
+        final actualValue = controller.getText();
+
+        expect(actualValue, isEmpty);
+      },
+    );
+
+    test(
+      'on input with a text element, expect the innerText value of the text element',
+      () async {
+        final input = '''
+  <navLabel>
+      <text>test</text>
+  </navLabel>
+  ''';
+        final controller = EpubNavigationInfoController.fromString(input);
+        final expectedValue = 'test';
+        final actualValue = controller.getText();
+
+        expect(actualValue, expectedValue);
+      },
+    );
+  });
 }
