cda2c2b052732e38002b16b4041bd96d1a95d887
getBoolean <danielwnewville@gmail.com>
2022-03-23 19:43:09 -0500

    add tests for Rootfile and RootfileList

---
+++ test/src/controller/container/rootfile_list_controller_test.dart
@@ -0,0 +1,72 @@
+import 'package:shu_epub/shu_epub.dart';
+import 'package:shu_epub/src/utils/xml_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+
+  group('fromXmlElement', () {
+    test(
+      'on input without rootfiles element, expect EpubException thrown',
+      () async {
+        final input = '''
+  <invalid></invalid>
+      ''';
+        final xmlDocument = XmlUtils.parseToXmlDocument(input);
+        final element = xmlDocument.firstElementChild!;
+  
+        expect(
+          () => RootfileList.fromXmlElement(element),
+          throwsA(isA<EpubException>()),
+        );
+      },
+    );
+  });
+  
+  group('fromString', () {
+    test(
+      'on input without rootfiles element, expect EpubException thrown',
+      () async {
+        final input = '''
+  <invalid></invalid>
+  ''';
+        expect(
+          () => RootfileList.fromString(input),
+          throwsA(isA<EpubException>()),
+        );
+      },
+    );
+  });
+
+  group('getItems', () {
+    test(
+      'on input without rootfile elements, expect empty list',
+      () async {
+        final input = '''
+  <rootfiles>
+  </rootfiles>
+  ''';
+        final controller = RootfileListController.fromString(input);
+        final actualValue = controller.getItems();
+  
+        expect(actualValue, isEmpty);
+      },
+    );
+  
+    test(
+      'on input with one rootfile element, expect a list of length 1 with it',
+      () async {
+        final input = '''
+  <rootfiles>
+      <rootfile>
+      </rootfile>
+  </rootfiles>
+  ''';
+        final controller = RootfileListController.fromString(input);
+        final expectedValue = [Rootfile()];
+        final actualValue = controller.getItems();
+  
+        expect(actualValue, expectedValue);
+      },
+    );
+  });
+}
