e52308b93bea6f25385b17d8148bb7c60b035db7
getBoolean <danielwnewville@gmail.com>
2022-03-26 16:35:26 -0500

    factory tests

--- test/src/features/package/controller/publication_metadata/epub_publication_metadata_controller_test.dart
+++ test/src/features/package/controller/publication_metadata/epub_publication_metadata_controller_test.dart
@@ -1,7 +1,42 @@
 import 'package:shu_epub/shu_epub.dart';
+import 'package:shu_epub/src/utils.dart';
 import 'package:test/test.dart';
 
 void main() {
+
+  group('fromXmlElement', () {
+    test(
+      'on input without metadata element, expect EpubException thrown',
+      () async {
+        final input = '''
+  <invalid></invalid>
+      ''';
+        final xmlDocument = XmlUtils.parseToXmlDocument(input);
+        final element = xmlDocument.firstElementChild!;
+  
+        expect(
+          () => EpubPublicationMetadataController.fromXmlElement(element),
+          throwsA(isA<EpubException>()),
+        );
+      },
+    );
+  });
+  
+  group('fromString', () {
+    test(
+      'on input without metadata element, expect EpubException thrown',
+      () async {
+        final input = '''
+  <invalid></invalid>
+  ''';
+        expect(
+          () => EpubPublicationMetadataController.fromString(input),
+          throwsA(isA<EpubException>()),
+        );
+      },
+    );
+  });
+
   group('getAllTitles', () {
     test(
       'on input without dc:title elements, expect empty list',
