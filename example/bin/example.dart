3580c4fb5412f403d8023036b14639cb7aabb0a9
getBoolean <danielwnewville@gmail.com>
2022-04-06 22:31:05 -0500

    Add extracted example

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,15 +1,42 @@
-import 'dart:io';
+import 'dart:async';
+import 'dart:io' as io;
+import 'dart:typed_data';
 import 'package:shu_epub/shu_epub.dart';
 import 'package:path/path.dart' as p;
 
 Future<void> main(List<String> arguments) async {
-  final filePath = p.join(Directory.current.path, 'assets', 'Guardians.epub');
+  await runArchiveExample();
+  await runExtractedExample();
+}
 
-  final File file = File(filePath);
+Future<void> runArchiveExample() async {
+  print('Reading from epub zip file...');
+  final filePath =
+      p.join(io.Directory.current.path, 'assets', 'Guardians.epub');
+
+  final io.File file = io.File(filePath);
 
   final bytes = await file.readAsBytes();
   final controller = EpubArchiveController(bytes);
   final EpubDetails? bookDetails = await controller.getDetails();
+  printBookDetails(bookDetails);
+}
+
+Future<void> runExtractedExample() async {
+  final directoryPath =
+      p.join(io.Directory.current.path, 'assets', 'Guardians');
+  print('Reading from epub extracted to filesystem...');
+  final controller = EpubExtractedController(io.Directory(directoryPath));
+  final bookDetails = await controller.getDetails();
+  if (bookDetails == null) {
+    print("Failed to read epub details.");
+    return;
+  }
+
+  printBookDetails(bookDetails);
+}
+
+void printBookDetails(EpubDetails? bookDetails) async {
   if (bookDetails == null) {
     print("Failed to read epub details.");
     return;
@@ -46,4 +73,34 @@ T? firstWhereOrElseNull<T>(List<T> items, bool Function(T) test) {
     }
   }
   return null;
+}
+
+class EpubExtractedController extends EpubControllerBase {
+  io.Directory rootDirectory;
+
+  EpubExtractedController(this.rootDirectory);
+
+  /// Read file from system
+  @override
+  FutureOr<Uint8List?> getFileBytes(String path) {
+    return io.File(p.join(rootDirectory.path, p.normalize(path))).readAsBytes();
+  }
+
+  @override
+  FutureOr<List<String>> getFilePaths() async {
+    var files = <io.FileSystemEntity>[];
+    var completer = Completer<List<io.FileSystemEntity>>();
+    var lister = rootDirectory.list(recursive: true);
+    lister.listen(
+      (file) => files.add(file),
+      onDone: () => completer.complete(files),
+    );
+
+    final completedFilesFound = await completer.future;
+    return completedFilesFound.map(
+      (io.FileSystemEntity fileSystemEntity) {
+        return fileSystemEntity.path.replaceFirst(rootDirectory.path + p.separator, '');
+      },
+    ).toList();
+  }
 }
