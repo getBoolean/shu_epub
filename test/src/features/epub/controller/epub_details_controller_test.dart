c0c8f6fbde3ec57012a499ee72666ca4b9655008
getBoolean <danielwnewville@gmail.com>
2022-04-08 15:02:31 -0500

    add tests

--- test/src/features/epub/controller/epub_details_controller_test.dart
+++ test/src/features/epub/controller/epub_details_controller_test.dart
@@ -2,11 +2,21 @@ import 'package:shu_epub/shu_epub.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('getContainer', () {
+  group('fromXmlString', () {
     test(
       'on input without a container element, expect a error thrown',
       () async {
-        final input = '''
+        final containerInput = '''
+<?xml version="1.0"?>
+<container>
+</container>
+''';
+        final packageInput = '''
+<?xml version="1.0"?>
+<invalid>
+</invalid>
+''';
+        final navigationInput = '''
 <?xml version="1.0"?>
 <invalid>
 </invalid>
@@ -14,15 +24,17 @@ void main() {
 
         expect(
           () => EpubDetailsController.fromXmlString(
-            containerContent: input,
-            packageContent: input,
-            navigationContent: input,
+            containerContent: containerInput,
+            packageContent: packageInput,
+            navigationContent: navigationInput,
           ),
           throwsA(isA<EpubException>()),
         );
       },
     );
+  });
 
+  group('getContainer', () {
     test(
       'on input with a container element, expect a container object',
       () async {
@@ -48,6 +60,70 @@ void main() {
         );
         final expectedValue = EpubContainer();
         final actualValue = controller.getContainer();
+
+        expect(actualValue, expectedValue);
+      },
+    );
+  });
+
+  group('getPackage', () {
+    test(
+      'on input with a package element, expect a package object',
+      () async {
+        final containerInput = '''
+<?xml version="1.0"?>
+<container>
+</container>
+''';
+        final packageInput = '''
+<?xml version="1.0"?>
+<package>
+</package>
+''';
+        final navigationInput = '''
+<?xml version="1.0"?>
+<ncx>
+</ncx>
+''';
+        final controller = EpubDetailsController.fromXmlString(
+          containerContent: containerInput,
+          packageContent: packageInput,
+          navigationContent: navigationInput,
+        );
+        final expectedValue = EpubPackage();
+        final actualValue = controller.getPackage();
+
+        expect(actualValue, expectedValue);
+      },
+    );
+  });
+
+  group('getNavigation', () {
+    test(
+      'on input with a navigation element, expect a navigation object',
+      () async {
+        final containerInput = '''
+<?xml version="1.0"?>
+<container>
+</container>
+''';
+        final packageInput = '''
+<?xml version="1.0"?>
+<package>
+</package>
+''';
+        final navigationInput = '''
+<?xml version="1.0"?>
+<ncx>
+</ncx>
+''';
+        final controller = EpubDetailsController.fromXmlString(
+          containerContent: containerInput,
+          packageContent: packageInput,
+          navigationContent: navigationInput,
+        );
+        final expectedValue = EpubNavigation();
+        final actualValue = controller.getNavigation();
 
         expect(actualValue, expectedValue);
       },
