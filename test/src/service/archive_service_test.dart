743b2dc5c248898369fc362f74f1b40f9ad0a928
getBoolean <danielwnewville@gmail.com>
2022-04-23 12:20:21 -0500

    Add ArchiveService tests

--- test/src/service/archive_service_test.dart
+++ test/src/service/archive_service_test.dart
@@ -2,6 +2,7 @@ import 'dart:io' as io;
 import 'dart:typed_data';
 
 import 'package:archive/archive.dart';
+import 'package:shu_epub/shu_epub.dart';
 import 'package:shu_epub/src/service.dart';
 import 'package:test/test.dart';
 
@@ -10,6 +11,46 @@ void main() {
 
   setUpAll(() async {
     bytes = await io.File('test/assets/Guardians.epub').readAsBytes();
+  });
+
+  group('isEpubFile', () {
+    test(
+      'on archive object which is an epub',
+      () async {
+        final archive = ArchiveService.decodeZip(bytes);
+        final actual = ArchiveService.isEpubFile(archive);
+        expect(actual, isTrue);
+      },
+    );
+    test(
+      'on archive object which is not an epub',
+      () async {
+        final bytes = await io.File('test/assets/test.zip').readAsBytes();
+        final archive = ArchiveService.decodeZip(bytes);
+        final actual = ArchiveService.isEpubFile(archive);
+        expect(actual, isFalse);
+      },
+    );
+  });
+
+  group('getMediaType', () {
+    test(
+      'on archive object which is an epub',
+      () async {
+        final archive = ArchiveService.decodeZip(bytes);
+        final actual = ArchiveService.getMediaType(archive).trim();
+        expect(actual, EpubMediaTypes.kEpubMimeType);
+      },
+    );
+    test(
+      'on archive object which is not an epub',
+      () async {
+        final bytes = await io.File('test/assets/test.zip').readAsBytes();
+        final archive = ArchiveService.decodeZip(bytes);
+        expect(() => ArchiveService.getMediaType(archive),
+            throwsA(isA<EpubException>()));
+      },
+    );
   });
 
   group('decodeZip', () {
