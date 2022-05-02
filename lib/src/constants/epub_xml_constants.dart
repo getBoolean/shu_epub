0e2d75b35a0c8a097b8631d408984223d7cb8665
getBoolean <danielwnewville@gmail.com>
2022-05-01 22:20:48 -0500

    Update epub_xml_constants.dart

--- lib/src/constants/epub_xml_constants.dart
+++ lib/src/constants/epub_xml_constants.dart
@@ -1,11 +1,25 @@
 part of shu_epub.constants;
 
+/// XML constants used in EPUB documents
 class EpubXMLConstants {
+  /// The namespace for the access mode meta element
   static const kAccessModeProperty = 'schema:accessMode';
+
+  /// The namespace for the accessibility feature meta element
   static const kAccessibilityFeatureProperty = 'schema:accessibilityFeature';
+
+  /// The namespace for the accessibility summary meta element
   static const kAccessibilitySummaryProperty = 'schema:accessibilitySummary';
+
+  /// The namespace for the accessibility hazard meta element
   static const kAccessibilityHazardProperty = 'schema:accessibilityHazard';
+
+  /// The namespace for the sufficient access mode meta element
   static const kAccessModeSufficientProperty = 'schema:accessModeSufficient';
+
+  /// The namespace for the Accessibility API meta element
   static const kAccessibilityAPIProperty = 'schema:accessibilityAPI';
+
+  /// The namespace for the Accessibility Control meta element
   static const kAccessibilityControlProperty = 'schema:accessibilityControl';
 }
